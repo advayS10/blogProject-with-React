b6f62c7df73ddf781ad145b8ebcc5b51e7cb937d
advayS10 <163100260+advayS10@users.noreply.github.com>
2024-06-25 13:08:48 +0530

    first commit

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+export default function Protected({children, authentication = true}) {
+    const [loader, setLoader] = useState(true)
+    const navigate = useNavigate()
+    const authStatus = useSelector((state) => state.auth.status)
+
+    useEffect(() => {
+        // if(authStatus === true){
+        //     navigate("/login")
+        // }
+        // else if(authStatus === false){
+        //     navigate("/")
+        // }
+
+        if(authentication && authStatus != authentication){
+            navigate("/login")
+        }
+        else if(!authentication && authStatus != authentication){
+            navigate("/")
+        }
+        setLoader(false)
+    }, [navigate, authStatus, authentication])
+
+    return loader ? <div>Loading...</div> : <>{children}</>
+}
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Container from './Container/Container'
+import Logo from './Logo'
+import LogoutBtn from './Header/LogoutBtn'
+import Select from './Select'
+import RTE from './RTE'
+import Signup from './Signup'
+import Login from './Login'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+import PostForm from './post-form/PostForm'
+import Button from './Button'
+import Input from './Input'
+
+export {
+    Header,
+    Footer, 
+    Container,
+    Logo,
+    LogoutBtn,
+    Select,
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    AuthLayout,
+    PostForm,
+    Button,
+    Input
+}
